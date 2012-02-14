0dfe9eff24fc935e07274c77384494c194ee58d3
Srijan Choudhary <srijan4@gmail.com>
2012-02-15 04:48:16 +0530

    Initial Commit. Simple displaying of releases works.

---
+++ releaseBot.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+# Created by Srijan Choudhary
+
+head = "==================== Release BOT ===================="
+foot = "====================================================="
+endl = "\r\n"
+
+import vh
+
+def header(text):
+    return endl + endl + head + endl + text
+
+def footer(text):
+    return text + endl + foot + endl
+
+help = """
+
+
+
+"""
+
+def getCategoriesList():
+    query = "SELECT `name` FROM `pi_rel_categories`"
+    res, rows = vh.SQL(query)
+    if res:
+        return rows
+    else:
+        return None
+
+def getReleasesByCategory(cat):
+    query = "SELECT `added_at`,`name`,`link`,`added_by` FROM `pi_releases` WHERE `category`='"+cat[0]+"' ORDER BY `added_at`"
+    res, rows = vh.SQL(query)
+    if res:
+        return rows
+    else:
+        return None
+
+
+def OnUserCommand(nick,data):
+    if data == "+uploads" or data == "+rel":
+        vh.pm(footer(header("Under Construction")),nick)
+        return 0
+
+    if data[:5] == "+rel ":
+        arg = data[5:].upper()
+        categories = getCategoriesList()
+
+        if arg == "ALL":
+            msg = ""
+            for cat in categories:
+                rels = getReleasesByCategory(cat)
+                if rels != None:
+                    msg += endl+cat[0]+":"+endl
+                    for rel in rels:
+                        msg += rel[0]+" : "+rel[1]+" [ "+rel[2]+" ] (By "+rel[3]+")"+endl
+            vh.pm(footer(header(msg)),nick)
+            return 0
+
+        for cat in categories:
+            if arg == cat[0]:
+                msg = endl+cat[0]+":"+endl
+                rels = getReleasesByCategory(cat)
+                if rels != None:
+                    for rel in rels:
+                        msg += rel[0]+" : "+rel[1]+" [ "+rel[2]+" ] (By "+rel[3]+")"+endl
+                else:
+                    msg += "No releases in this category"+endl
+                vh.pm(footer(header(msg)),nick)
+                return 0
+
+        msg = endl+"Usage:"+endl+endl+"+rel <category_name>"+endl+endl+"Where <category_name> is one of:"+endl+endl
+        for cat in categories:
+            msg += cat[0]+endl
+        vh.pm(footer(header(msg)),nick)
+        return 0
+    return 0
