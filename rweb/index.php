ae87159b0139b52bfb868dceec32bc80dcad7118
Srijan Choudhary <srijan4@gmail.com>
2012-02-19 00:44:05 +0530

    Releases are now displayed in descending order.

--- rweb/index.php
+++ rweb/index.php
@@ -72,7 +72,7 @@ function bytesToSize($bytes, $precision = 2) {
     $query = "SELECT `id`,`text`,`added_by`
               FROM `pi_releases`
               WHERE `category`='".$category['name']."'
-              ORDER BY `added_at`";
+              ORDER BY `added_at` DESC";
     $result_set = mysql_query($query);
     if(mysql_num_rows($result_set) > 0) {
       echo "
